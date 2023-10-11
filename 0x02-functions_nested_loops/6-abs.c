fe8a52ff1e9b6428272b83ddaa2a521545f3df1b
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-10-11 14:38:23 -0700

    C is a procedural language

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
+ * _abs - printing absolute value
+ * @n: The data type int to be checked
  *
- *
- *
- *
- *
+ * Return: n (If n is greater than 0)
  */
 int _abs(int n)
 {
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	char minutes, hours;
+
+	for (hours = 0; hours <= 24; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hours);
+			_putchar(hours);
+
+			_putchar(':');
+			_putchar(minutes);
+			_putchar(minutes);
+			_putchar('\n');
+		}
+	}
+}
