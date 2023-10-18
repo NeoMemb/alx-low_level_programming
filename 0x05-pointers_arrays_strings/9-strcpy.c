a375f5314f085929d3e8d379d23ca1b6f094613f
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-10-17 18:56:11 -0700

    Pointer Array & Strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - Copies the string pointed to by src.
+ * @dest: Type char pointer
+ * @src: Type char pointer
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	int c;
+
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[c] = src[c];
+	}
+	dest[c] = '\0';
+
+	return (dest);
+}
