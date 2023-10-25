3e5905c1fbfa2b446eb4d1cb20fce5952a1db5eb
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-10-25 14:58:24 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-	char str;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s == '\0')
 	{
-		str = s[i];
-		_putchar(str);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
