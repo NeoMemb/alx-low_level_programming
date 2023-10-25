628bc8e67c7e4011b50d9e38226c674c328cfeab
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-10-25 15:52:32 -0700

    Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,12 @@
 #include "main.h"
 /**
- *********/
+ * factorial - mathematical factorial
+ * @n: int data type
+ * Description: The function return the value of n *
+ * the value of the factorial of n (recursively)
+ *
+ * Return: n * factorial(n - 1)
+ */
 int factorial(int n)
 {
 	if (n > 0)
