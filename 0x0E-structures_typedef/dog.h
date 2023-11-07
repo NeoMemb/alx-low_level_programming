47bf31ec53a535ed595727ed9924a5e2d444e55a
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-11-07 04:39:21 -0800

    Using betty complaint on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef _DOG_
 #define _DOG_
+/**
+ * struct dog - short description
+ * @name: pointer to char
+ * @age: float data type
+ * @owner: pointer to char
+ * Description: name is a string as name
+ * of dog. age is age of dog. owner is
+ * name of dog owner
+ */
 struct dog
 {
 	char *name;
