92d2336eeac8e1a2fcb5b9ddfb5d93a6ec1d711d
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-11-07 18:08:25 -0800

    We are closinbg

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@
  */
 void print_dog(struct dog *d)
 {
-	(d == NULL) ? (printf("Name: (nil)\n")) : (printf("Name: %s\n", d->name));
+	(d->name == NULL) ? (printf("Name: (nil)\n")) : (printf("Name: %s\n", d->name));
 	printf("Age: %f\n", d->age);
-	(d == NULL) ? (printf("Owner: (nil)\n")) : (printf("Owner: %s\n", d->owner));
+	(d->owner == NULL) ? (printf("Owner: (nil)\n")) : (printf("Owner: %s\n", d->owner));
 }
