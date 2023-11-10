ba9fd4df6b2ab3bb46040e6334e44319ca6040ee
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-11-09 18:29:57 -0800

    Task 3 is ready to be checked with the given checker

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/**
+ * op_add - addition
+ * @a: int data type
+ * @b: int data type
+ * Return: addition of a and b
+ *
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substraction
+ * @a: int data type
+ * @b: int data type
+ * Return: substraction of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_div - division
+ * @a: int data type
+ * @b: int data type
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mul - multiplication
+ * @a: int data type
+ * @b: int data type
+ * Return: multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_mod - remainder
+ * @a: int data type
+ * @b: int data type
+ * Return: modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
