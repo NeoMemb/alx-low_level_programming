b6092703d6fc18d81747676744b88f2683e985c7
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-11-20 03:38:53 -0800

    Task 0 output is good and it's ready to be checked

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints the data in the list
+ * @h: pointer to node, which is also the head of the linked list
+ * Return: count, number of data transversed
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
