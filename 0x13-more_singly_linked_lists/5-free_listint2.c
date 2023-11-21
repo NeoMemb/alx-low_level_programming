976c4e3efa9830a3240fbd27e634776468cfce97
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-11-21 08:33:51 -0800

    Task 5 is ready to be checked

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: the beginning of the pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		listint_t *next_ptr;
+
+		next_ptr = ptr->next;
+		free(ptr);
+		ptr = next_ptr;
+	}
+	*head = NULL;
+}
