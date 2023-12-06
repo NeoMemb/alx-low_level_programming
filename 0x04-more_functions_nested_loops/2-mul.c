4d8ab8feb6ac98ab58c93072fdff45cb8b286b4d
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-12-06 09:12:31 -0800

    while(task 0,1), check task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Multiplying two given integer
+ * @a: first integer to be multiplied
+ * @b: seconf integer to be multiplied
+ * Return: the result of the multiplied int
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
