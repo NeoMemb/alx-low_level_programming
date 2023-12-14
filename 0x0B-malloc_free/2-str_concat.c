63a91510c3be2fd74c644dce7fec6874ebe820d1
Ariori Abdulrafiu <abdulrafiuariori@gmail.com>
2023-12-14 00:35:42 -0800

    Change the condition if output is NULL, to free mem

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * str_concat - concatenates string
+ * str_concat - concatenates two string
  * @s1: char type
  * @s2: char type
- * Description: s1 and s2 are concatenated
+ * Description: s1 and s2 are to be concatenated
  * together, and the concatenated string is returned.
- *
+ * The returned pointer should point to a newly allocated space in memory
+ * which contains the contents of s1, followed by
+ * the contents of s2, and null terminated
  * Return: char (concatenated string)
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, tot_length, x, y;
+	unsigned int s1_len, s2_len, tot_length, x, y;
 	char *output = NULL;
 
 	if (s1 == NULL)
@@ -23,27 +25,27 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
 	{
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
 	{
 	}
-	tot_length = i + j + 1;
+	tot_length = s1_len + s2_len + 1;
 	output = malloc(tot_length * sizeof(*output));
-	for (x = 0; x < i; x++)
+	for (x = 0; x < s1_len; x++)
 	{
 		output[x] = s1[x];
 	}
-	for (y = 0; y < j; y++, x++)
+	for (y = 0; y < s2_len; y++, x++)
 	{
 		output[x] = s2[y];
 	}
 	output[tot_length] = s2[y];
 	if (output == NULL)
 	{
+		free(output);
 		return (NULL);
 	}
-
 	return (output);
 }
